e39b8f706c1fd29845628ba328cbea18d0f2debb
mohamed ait taleb <mohamedaittaleb1994@gmail.com>
2019-04-17 00:13:37 +0100

    edit my role

--- ansible/roles/my_role/test/integration/default/serverspec/default_spec.rb
+++ ansible/roles/my_role/test/integration/default/serverspec/default_spec.rb
@@ -4,3 +4,13 @@ describe user('dev') do
     it { should exist }
     it { should belong_to_group 'devopsmeetup' }
 end
+
+describe service('apache2') do
+  it { should be_enabled }
+  it { should be_running }
+end
+
+
+describe file('/var/www/index.html') do
+  it { should be_file }
+end
